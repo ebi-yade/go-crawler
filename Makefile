8b383784b60045aba7b8d935978b72203c3382dd
ebi-exp <ebi.exp@gmail.com>
2020-08-13 14:05:15 +0900

    [add] editor config & vagrant-remake

--- Makefile
+++ Makefile
@@ -3,16 +3,23 @@ vagrant-up:
 	cd ../vagrant && \
 	vagrant up && \
 	cd ../src
+vagrant-halt:
+	cd ../vagrant && \
+	vagrant halt && \
+	cd ../src
 vagrant-reload:
 	cd ../vagrant && \
 	cp Vagrantfile.bak Vagrantfile && \
 	sed -i.bak -e "s/VmBoxNameHere/amzn2-$(PROJECT_NAME)/g" Vagrantfile && \
 	vagrant reload --provision && \
 	cd ../src
+vagrant-remake:
+	cd ../vagrant && \
+	vagrant destroy && \
+	cp Vagrantfile.bak Vagrantfile && \
+	sed -i.bak -e "s/VmBoxNameHere/amzn2-$(PROJECT_NAME)/g" Vagrantfile && \
+	vagrant up && \
+	cd ../src
 vagrant-ssh:
 	cd ../vagrant && \
 	vagrant ssh
-vagrant-halt:
-	cd ../vagrant && \
-	vagrant halt && \
-	cd ../src
--- cmd/go-crawler/main.go
+++ cmd/go-crawler/main.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+var (
+	_url = "https://transit.yahoo.co.jp/traininfo/area/4/"
+)
+
 func main() {
-	doc, err := goquery.NewDocument("https://github.com/PuerkitoBio/goquery")
+	doc, err := goquery.NewDocument(_url)
 	if err != nil {
-		fmt.Print("url scarapping failed")
+		panic(err)
 	}
-	doc.Find("table > tbody > tr > td.content > span > a").Each(func(_ int, s *goquery.Selection) {
-		url, _ := s.Attr("href")
-		fmt.Println(url)
-	})
+	u := url.URL{}
+	u.Scheme = doc.Url.Scheme
+	u.Host = doc.Url.Host
+
+	fmt.Println(u.Scheme)
+	fmt.Println(u.Host)
+
+	title := doc.Find("title").Text()
+	fmt.Println(title)
 }
